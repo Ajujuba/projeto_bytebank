cc5905292cb35312e5b41ffa40dd9bc4dcc9f029
Ana Julia Terra <ajtalves@hotmail.com>
2021-01-21 20:16:35 -0300

    Projeto Inicial

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    body: Column(
+      children: <Widget>[
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('100.0'),
+            subtitle: Text('1000'),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('200.0'),
+            subtitle: Text('1000'),
+          ),
+        ),
+      ],
+    ),
+    appBar: AppBar(
+      title: Text('Transferências'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Icon(Icons.add),
+    ),
+  ),
+));
